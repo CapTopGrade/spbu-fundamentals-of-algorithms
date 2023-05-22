857cc3e5490f0e4a77fc1b3872beff66e3c31582
Datsyk R.V <128227860+CapTopGrade@users.noreply.github.com>
2023-05-22 17:13:12 +0300

    Update convex_bucket.py

--- practicum_5/homework/convex_bucket.py
+++ practicum_5/homework/convex_bucket.py
@@ -8,13 +8,25 @@ from src.plotting import plot_points
 
 def convex_bucket(points: NDArray) -> NDArray:
     """Complexity: O(n log n)"""
-    clockwise_sorted_ch = []
 
-    ##########################
-    ### PUT YOUR CODE HERE ###
-    ##########################
+    # Sort points by x-coordinate
+    sorted_points = sorted(points, key=lambda p: (p[0], p[1]))
 
-    return np.array(clockwise_sorted_ch)
+    # Initialize stack with first two points
+    stack = [sorted_points[0], sorted_points[1]]
+
+    # Iterate over remaining points
+    for p in sorted_points[2:]:
+        # Remove points from top of stack until slope is non-positive
+        while len(stack) > 1 and (p[1] - stack[-1][1]) * (stack[-1][0] - stack[-2][0]) <= (
+                stack[-1][1] - stack[-2][1]) * (p[0] - stack[-1][0]):
+            stack.pop()
+
+        # Add current point to stack
+        stack.append(p)
+
+    # Return clockwise order of points in stack
+    return np.array(stack + stack[-2::-1])
 
 
 if __name__ == "__main__":
